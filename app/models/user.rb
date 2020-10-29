b0c15babf8bbef24a52ab6327ff45a8633b2b8c6
imoto <namamisomisa@outlook.jp>
2020-10-29 14:46:12 +0000

    7. 1. 3 ログイン状態の永続的保持

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,5 +32,10 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end  
+  end 
+  
+  # トークンがダイジェストと一致すればtrueを返します。
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
