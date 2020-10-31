b946bccf60beaeb6fd78f226e48970acf2cedbf7
imoto <namamisomisa@outlook.jp>
2020-10-31 14:32:03 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,23 +4,24 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
   # 永続的セッションを破棄します
   def forget(user)
     user.forget # Userモデル参照
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
+
   # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -44,4 +45,3 @@ module SessionsHelper
     !current_user.nil?
   end
 end
-  
