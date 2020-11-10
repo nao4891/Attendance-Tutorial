330576cda8ec5a2237a84242dd731c4b3feabe23
imoto <namamisomisa@outlook.jp>
2020-11-10 14:02:19 +0000

    9. 2. 2 表示に対応しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,11 +46,10 @@ class UsersController < ApplicationController
   end
 
   private
-
+  
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-
     # beforeフィルター
 
     # paramsハッシュからユーザーを取得します。
