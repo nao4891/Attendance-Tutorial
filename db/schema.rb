be62ea529070d6c54334c3a63e28f763ba460456
imoto <namamisomisa@outlook.jp>
2020-11-20 19:26:25 +0000

    10. 6 退勤登録ボタンを作ろう

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20201117131934) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-11-09 23:00:00"
-    t.datetime "work_time", default: "2020-11-09 22:30:00"
+    t.datetime "basic_time", default: "2020-11-20 23:00:00"
+    t.datetime "work_time", default: "2020-11-20 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
